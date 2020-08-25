dc31247329de07c7fca3bfb6ed7e1c7d4450cfa4
Romario Gustave <romagustave10@gmail.com>
2020-08-25 22:16:55 +0000

    W

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,15 @@ class ApplicationController < Sinatra::Base
       !!current_user
     end
 
+    def login(email, password)
+      user = User.find_by(:email => email)
+      if user && user.authenticate(password)
+        session[:email] = user.email
+      else
+        redirect '/login'
+      end
+    end
+
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
