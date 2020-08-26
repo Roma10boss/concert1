1974667479f64c8bbabdec4aed8e85f67aa6c9a0
Romario Gustave <romagustave10@gmail.com>
2020-08-26 19:55:31 +0000

    Encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :artists
   has_many :concerts, through: :artists
+  has_secure_password
+
 
   validates :email, :password, :username, presence: true
   validates :email, :username, uniqueness: true
